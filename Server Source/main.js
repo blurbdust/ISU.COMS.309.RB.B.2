b5c4be25bc405abbddbb03acbd185675c57580a0
Nic Losby <nlosby@iastate.edu>
2017-09-07 21:30:49 -0500

    Got nodejs working

--- Server Source/main.js
+++ Server Source/main.js
@@ -1,6 +1,6 @@
 const electron = require('electron');
-const app = require('app');
-const BrowserWindow = require('browser-window');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
 const path = require('path');
 const url = require('url');
