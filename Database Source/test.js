73a2970bac66c98d1da7004fd566e6b9b55b3fed
Gabriel Stackhouse <gabes@iastate.edu>
2017-10-02 14:01:01 -0500

    Added public directory.  Made the HTML for Create Account

--- Database Source/test.js
+++ Database Source/test.js
@@ -2,11 +2,16 @@ var express = require("express");
 var app = express();
 var port = 3000;
  
-app.use("/", (req, res) => {
+app.get("/login", (req, res) => {
  res.sendFile(__dirname + "/login.html");
 }); 
 
-app.use('/public', express.static(__dirname + '/public'));
+app.get("/create_account", (req, res) => {
+ res.sendFile(__dirname + "/create_account.html");
+}); 
+
+//Public folder to serve files
+app.use(express.static(__dirname + '/public'));
  
 app.listen(port, () => {
  console.log("Server listening on port " + port);
