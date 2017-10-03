2399d67da5f563dfd59683b1aeadd56ea4637660
Gabriel Stackhouse <gabes@iastate.edu>
2017-10-02 20:18:52 -0500

    Altered table to not allow duplicate usernames. Form now passing post data to server

--- Database Source/Create Users Table.sql
+++ Database Source/Create Users Table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE users (
 	ID MEDIUMINT NOT NULL AUTO_INCREMENT, /* The user ID.  Starts at 1 and increments from there.*/
-	Username varchar(25) NOT NULL,
+	Username varchar(25) NOT NULL UNIQUE,
 	Password varchar(40) NOT NULL,
-	UserRole TINYINT NOT NULL, /* Integer representing user role. 0 = admin, 1 = Driver, 2 = Camera/Shooter,  3 = Spectator */
+	UserRole TINYINT, 	/* Integer representing user role. 1 = admin, 2 = Driver, 3 = Camera/Shooter,  4 = Spectator */
     DisplayName varchar(25),
-    Bio varchar(140), /* Bio of player, with imposed 140-character limit */
+    Bio varchar(140), /* Bio of player (140-character limit) */
 	PRIMARY KEY (ID)
 );
--- Database Source/test.js
+++ Database Source/test.js
@@ -4,13 +4,18 @@ var port = 3000;
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
 
+
+//Initialize bodyParser
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
  
+ 
+//Send login page 
 app.get("/login", (req, res) => {
  res.sendFile(__dirname + "/login.html");
 }); 
 
+//Send Create User page
 app.get("/create_account", (req, res) => {
  res.sendFile(__dirname + "/create_account.html");
 }); 
@@ -18,12 +23,9 @@ app.get("/create_account", (req, res) => {
 //Public folder to serve files
 app.use(express.static(__dirname + '/public'));
  
+//Listen on port
 app.listen(port, () => {
  console.log("Server listening on port " + port);
-});
-
-app.post('/login', function(req, res) {
-	
 });
 
 var con = mysql.createConnection({
@@ -36,4 +38,26 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+});
+
+app.post('/login', function(req, res) {
+	var username = req.body.uname;
+	var password = req.body.psw;
+	
+	console.log("Username: " + username);
+	console.log("Password: " + password);
+	
+	
+});
+
+app.post('/create_account', function(req, res) {
+	var username = req.body.uname;
+	var password = req.body.psw;
+	var confirmPassword = req.body.confirmPsw;
+	
+	console.log("Username: " + username);
+	console.log("Password: " + password);
+	console.log("Confirm Password: " + confirmPassword);
+	
+	
 });
