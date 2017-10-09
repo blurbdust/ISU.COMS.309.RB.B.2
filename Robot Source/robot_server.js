698d5b49dbe3a5975dfc2f44c5cb50d9cd6ff3ec
Kevin Haan <khaan@iastate.edu>
2017-10-08 19:49:51 -0500

    Made changes to RPi server code

--- Robot Source/robot_server.js
+++ Robot Source/robot_server.js
@@ -1,14 +1,18 @@
 var io_RPI = require("socket.io").listen(521); //Operators connect to this
 var io = require("socket.io-client");	//Central Server connection
 var sleep = require('system-sleep');
+/*
+var RaspiCam = require("raspicam");
+var camera = new RaspiCam({ opts });
+*/
 
 
 
-var io_CS = io.connect('proj-309-am-b-4.cs.iastate.edu:80');
+var io_CS = io.connect('proj-309-rb-b-4.cs.iastate.edu:80');
 var operator;
 
 var SerialPort = require('serialport');
-var serialPort = new SerialPort('COM3',{ 
+var serialPort = new SerialPort("/dev/ttyACM0",{ 
 	baudrate: 9600,
 	dataBits: 8, 
     parity: 'none', 
@@ -29,11 +33,12 @@ io_CS.on('connect', function(){
 
 io_RPI.on('connection', function(socket){
 	console.log("Client Connected");
+	console.log(socket.handshake.address);
+	
 	socket.on('Serial Movement', function(data){
-		console.log(data.dir)
 		serialPort.write(data.dir);
 	});
-	
+	start
 	socket.on('disconnect', function () {
       console.log('A user disconnected');
 	});
