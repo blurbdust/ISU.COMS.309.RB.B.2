be9301f3d8546c54cca1b6d9dd11668e5f9808a8
Nic Losby <nlosby@iastate.edu>
2017-11-04 22:16:44 -0500

    Fixed the gunner and driver pages, my bad

--- Server/public/gunner.js
+++ Server/public/gunner.js
@@ -1,8 +1,37 @@
+var socket_server = connectToUserSocket();
+var robot_ip = "http://raspberrypi3-a.student.iastate.edu";
+/*
+socket_server.on('Robot Address', function(data){
+  robot_ip = data.ip;
+});
+*/
 
-var socket = io('http://localhost:521');
+//Redirect user as instructed by server
+socket_server.on('redirect', function(destination) {
+  window.location.href = destination;
+});
+
+var socket_robot = io(robot_ip + ':5210');
+
+
+//Chat Box
+$(function () {
+  $('#msgform').submit(function(){
+    socket_server.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
+  });
+  socket_server.on('chat message', function(msg){
+    $('#messages').append($('<li><strong>' + msg.username + ":</strong> " + msg.message + '</li>'));
+    var chatDiv = document.getElementById("chat-box");
+    chatDiv.scrollTop = chatDiv.scrollHeight;
+  });
+});
+
+
 
 window.addEventListener("load", function(){
-	
+  
   var buttonUp = document.getElementById('buttonUp');
   var buttonLeft = document.getElementById('buttonLeft');
   var buttonRight = document.getElementById('buttonRight');
@@ -11,52 +40,66 @@ window.addEventListener("load", function(){
 
   buttonUp.addEventListener('mousedown', function() {
       console.log("I");
-      socket.emit('Serial Movement', { dir: 'I'});
+      socket_robot.emit('Serial Movement', { dir: 'I'});
   });
   buttonUp.addEventListener('mouseup', function() {
       console.log("i");
-      socket.emit('Serial Movement', { dir: 'i'});
+      socket_robot.emit('Serial Movement', { dir: 'i'});
   });
   
   
   
   buttonLeft.addEventListener('mousedown', function() {
       console.log("J");
-      socket.emit('Serial Movement', { dir: 'J'});
+      socket_robot.emit('Serial Movement', { dir: 'J'});
   });
   buttonLeft.addEventListener('mouseup', function() {
       console.log("j");
-      socket.emit('Serial Movement', { dir: 'j'});
+      socket_robot.emit('Serial Movement', { dir: 'j'});
   });
   
   
   
   buttonRight.addEventListener('mousedown', function() {
       console.log("L");
-      socket.emit('Serial Movement', { dir: 'L'});
+      socket_robot.emit('Serial Movement', { dir: 'L'});
   });
   buttonRight.addEventListener('mouseup', function() {
       console.log("l");
-      socket.emit('Serial Movement', { dir: 'l'});
+      socket_robot.emit('Serial Movement', { dir: 'l'});
   });  
   
   
   buttonDown.addEventListener('mousedown', function() {
       console.log("M");
-      socket.emit('Serial Movement', { dir: 'M'});
+      socket_robot.emit('Serial Movement', { dir: 'M'});
   });
   buttonDown.addEventListener('mouseup', function() {
       console.log("m");
-      socket.emit('Serial Movement', { dir: 'm'});
+      socket_robot.emit('Serial Movement', { dir: 'm'});
   });
 
   buttonFire.addEventListener('mousedown', function() {
       console.log("K");
-      socket.emit('Serial Movement', { dir: 'K'});
+      socket_robot.emit('Serial Movement', { dir: 'K'});
   });
   buttonFire.addEventListener('mouseup', function() {
-      console.log("k");
-      socket.emit('Serial Movement', { dir: 'k'});
+      console.log("K");
+      socket_robot.emit('Serial Movement', { dir: 'k'});
   });
   
+  var webcam_addr = "raspberrypi3-a.student.iastate.edu";
+  var webcam_port = "12000";
+  var webcam_host = $(".feed img");
+  var cam_socket = io.connect('http://' + webcam_addr + ':' + webcam_port);
+
+
+  cam_socket.on("connection", function(socket){
+    console.log("Connected");
+  });
+
+  cam_socket.on('image', function (data) {
+    webcam_host.attr("src", "data:image/jpeg;base64," + data );
+  });
+
 });
