c80f16d90757e35e32ff92b01e61c5fdcdbfc125
Gabriel Stackhouse <gabes@iastate.edu>
2017-12-05 03:01:44 -0600

    Implementing profile picture upload

--- Server/public/profile.js
+++ Server/public/profile.js
@@ -10,57 +10,63 @@ socket.emit('request profile info', getCookie("username"));
 
 socket.on('profile info', function(data) {
 	
-	document.getElementById('username').innerHTML = data.username;
-	document.getElementById('displayName').innerHTML = data.displayName;
-	document.getElementById('bio').innerHTML = data.bio;
+	if (data.success) {
 	
-	if (data.onlineStatus == true) {
-		document.getElementById('onlineStatus').innerHTML = "Online";
-		document.getElementById('onlineStatus').style.color = "green";
+		document.getElementById('username').innerHTML = data.username;
+		document.getElementById('displayName').innerHTML = data.displayName;
+		document.getElementById('bio').innerHTML = data.bio;
+		
+		if (data.onlineStatus == true) {
+			document.getElementById('onlineStatus').innerHTML = "Online";
+			document.getElementById('onlineStatus').style.color = "green";
+		}
+		else {
+			document.getElementById('onlineStatus').innerHTML = "Offline";
+			document.getElementById('onlineStatus').style.color = "red";
+		}
+		
+		//Set edit buttons
+		if (data.username == getCookie("username")) {
+			document.getElementById('displayNameButton').innerHTML = "<button onclick='editDisplayName();'>Edit</button>";
+			document.getElementById('bioButton').innerHTML = "<button onclick='editBio();'>Edit</button>";
+		}
+		else {
+			document.getElementById('displayNameButton').innerHTML = "";
+			document.getElementById('bioButton').innerHTML = "";
+		}
+		
+		//Set profile picture
+		var urlBase = "http://proj-309-rb-b-2.cs.iastate.edu:3000/avatars/";
+		var url = "";
+		if (urlExists(urlBase + data.ID + ".png")) {
+			url = urlBase + data.ID + ".png";
+		}
+		else if (urlExists(urlBase + data.ID + ".jpg")) {
+			url = urlBase + data.ID + ".jpg";
+		}
+		else if (urlExists(urlBase + data.ID + ".jpeg")) {
+			url = urlBase + data.ID + ".jpeg";
+		}
+		else {
+			url = urlBase + "default.jpg";
+		}
+		document.getElementById('profilePic').innerHTML = '<img src="' + url + '" class="avatar" alt="Profile Image"/>'
 	}
 	else {
-		document.getElementById('onlineStatus').innerHTML = "Offline";
-		document.getElementById('onlineStatus').style.color = "red";
+		alert("User does not exist.");
 	}
-	
-	//Set edit buttons
-	if (data.username == getCookie("username")) {
-		document.getElementById('displayNameButton').innerHTML = "<button onclick='editDisplayName();'>Edit</button>";
-		document.getElementById('bioButton').innerHTML = "<button onclick='editBio();'>Edit</button>";
-	}
-	else {
-		document.getElementById('displayNameButton').innerHTML = "";
-		document.getElementById('bioButton').innerHTML = "";
-	}
-	
-	//Set profile picture
-	var urlBase = "http://proj-309-rb-b-2.cs.iastate.edu:3000/avatars/";
-	var url = "";
-	if (urlExists(urlBase + data.ID + ".png")) {
-		url = urlBase + data.ID + ".png";
-	}
-	else if (urlExists(urlBase + data.ID + ".jpg")) {
-		url = urlBase + data.ID + ".jpg";
-	}
-	else if (urlExists(urlBase + data.ID + ".jpeg")) {
-		url = urlBase + data.ID + ".jpeg";
-	}
-	else {
-		url = urlBase + "default.jpg";
-	}
-	document.getElementById('profilePic').innerHTML = '<img src="' + url + '" class="avatar" alt="Profile Image"/>'
 });
 
 function editDisplayName() {
 	var displayName = prompt("Enter your new display name:", "Harry Potter");
-	displayName = displayName.replace("'", "\'");
+	displayName = displayName.replace("\"","\\'");
 	socket.emit ('edit display name', displayName);
 	socket.emit('request profile info', getCookie("username"));
 }
 
 function editBio() {
 	var bio = prompt("Enter your new bio:", "I am the chosen one!");
-	bio.replace("'","\\'"); //TODO - Fix single quotes
+	bio = bio.replace("\"","\\'");
 	socket.emit ('edit bio', bio);
 	socket.emit('request profile info', getCookie("username"));
 }
@@ -71,6 +77,45 @@ function urlExists(url) {
     http.open('HEAD', url, false);
     http.send();
     return http.status!=404;
+}
+
+function viewProfile() {
+	var uname = prompt("Enter username:", getCookie("username"));
+	socket.emit('request profile info', uname);
+}
+
+function goToLobby() {
+	window.location.href = 'http://proj-309-rb-b-2.cs.iastate.edu:3000/lobby';
+}
+
+function logout() {
+	window.location.href = 'http://proj-309-rb-b-2.cs.iastate.edu:3000/login';
+}
+
+function openModal() {
+	document.getElementById('myModal').style.display = "block";
+}
+
+document.getElementsByClassName("close")[0].onclick = function() {
+    document.getElementById('myModal').style.display = "none";
+}
+
+function uploadProfilePic() {
+	
+	//Convert to Base64
+	var file = document.getElementById('newProfilePic').files[0];
+	var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+      console.log(reader.result);
+    };
+    reader.onerror = function (error) {
+      console.log('Error: ', error);
+    };
+	
+	var toSend = {"image":reader.result, "ID":getCookie("ID")};
+	
+	
 }
 		
 		
