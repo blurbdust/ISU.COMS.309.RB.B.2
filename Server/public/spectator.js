05ad46a389992df347582a63d89021edab81abbd
Gabriel Stackhouse <gabes@iastate.edu>
2017-12-06 01:50:13 -0600

    Spectator functionality

--- Server/public/spectator.js
+++ Server/public/spectator.js
@@ -12,6 +12,7 @@ socket_server.on('redirect', function(destination) {
 });
 
 var socket_robot = io(robot_ip + ':5210');
+var cur_addr = "raspberrypi3-a.student.iastate.edu";
 
 
 //Chat Box
@@ -31,13 +32,16 @@ $(function () {
 
 
 window.addEventListener("load", function(){
-
-  var webcam_addr = "raspberrypi3-a.student.iastate.edu";
+  
+  
+  var webcam_addr = cur_addr;
   var webcam_port = "12000";
   var webcam_host = $(".feed img");
   var cam_socket = io.connect('http://' + webcam_addr + ':' + webcam_port);
   var logout = document.getElementById('logout');
   var lobby = document.getElementById('lobby');
+  var right = document.getElementById('rightRobotSwitch');
+  var left = document.getElementById('leftRobotSwitch');
 
 
   cam_socket.on("connection", function(socket){
@@ -58,5 +62,62 @@ window.addEventListener("load", function(){
     console.log("Redirecting back to lobby");
     window.location.href = 'http://proj-309-rb-b-2.cs.iastate.edu:3000/lobby';
   });
+  
+    left.addEventListener('mouseup', function() {
+	    console.log("Toggling left");
+		var cornelius = "raspberrypi3-a.student.iastate.edu";
+		var mrRobot = "monmodenic.student.iastate.edu";
+		
+		if (cur_addr == cornelius)
+			cur_addr = mrRobot;
+		else if (cur_addr == mrRobot)
+			cur_addr = cornelius;
+		
+		var webcam_addr = cur_addr;
+	});
+	
+	right.addEventListener('mouseup', function() {
+	    console.log("Toggling right");
+		
+		var cornelius = "raspberrypi3-a.student.iastate.edu";
+		var mrRobot = "monmodenic.student.iastate.edu";
+		
+		if (cur_addr == cornelius)
+			cur_addr = mrRobot;
+		else if (cur_addr == mrRobot)
+			cur_addr = cornelius;
+		
+		var webcam_addr = cur_addr;
+		
+	});
 
 });
+
+
+
+function switchCamera() {
+	document.getElementById('video').src = "";
+	
+	/*var cornelius = "raspberrypi3-a.student.iastate.edu";
+	var mrRobot = "monmodenic.student.iastate.edu";
+	
+	if (cur_addr == cornelius)
+		cur_addr = mrRobot;
+	else if (cur_addr == mrRobot)
+		cur_addr = cornelius;
+	
+    var webcam_addr = cur_addr;
+    var webcam_port = "12000";
+    var webcam_host = $(".feed img");
+    var cam_socket = io.connect('http://' + webcam_addr + ':' + webcam_port);
+
+    cam_socket.on("connection", function(socket){
+      console.log("Connected");
+    });
+
+    cam_socket.on('image', function (data) {
+      webcam_host.attr("src", "data:image/jpeg;base64," + data );
+    });
+	
+	
+}
